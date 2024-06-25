bad0409de00248142e86cecc2ddeaa6141400ab3
Nilesh Nagare <nnilesh2@gmail.com>
2024-06-25 07:00:26 +0000

    added nginx on kind cluster in ec2

---
+++ K8s/ec2/ec2_user_data_kind_cluster.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Update the system and install necessary packages
+sudo dnf update -y
+
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo mv kubectl /usr/local/bin/
+
+# Install Docker
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+# Install Kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.23.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+sudo kind create cluster
---
+++ K8s/nginx/create_deployment.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+kubectl apply -f nginx-deployment.yaml
+kubectl apply -f nginx-service.yaml
